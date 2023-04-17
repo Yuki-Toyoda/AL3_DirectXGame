d58c1bcca7cf993fef827b9eae342b1a0b6d3adc
夕輝 豊田 <K022G0030@g.neec.ac.jp>
2023-04-17 14:31:35 +0900

    変更をもとに戻す

--- main.cpp
+++ main.cpp
@@ -20,7 +20,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	// ゲームウィンドウの作成
 	win = WinApp::GetInstance();
-	win->CreateGameWindow(L"LE2A_11_トヨダ_ユウキ_AL3");
+	win->CreateGameWindow(L"DirectXGame");
 
 	// DirectX初期化処理
 	dxCommon = DirectXCommon::GetInstance();
